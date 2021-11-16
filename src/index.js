23163f7d079469852a3116ab4a82920dcd971c15
Oleh Semennykov <sumcue@gmail.com>
2021-11-16 20:31:02 +0200

    start fetching

--- src/index.js
+++ src/index.js
@@ -10,15 +10,61 @@ const refs = {
     countryInfo : document.querySelector(".country-info"),
 };
 
-// console.log(refs.searchBox);
-// console.log(refs.countryList);
+refs.searchBox.addEventListener('input', debounce((handleCountryInput),DEBOUNCE_DELAY))
 
-// refs.searchBox.addEventListener('input', )
+function handleCountryInput(evt) {
+    const countryName = evt.target.value;
+fetchCountries(countryName)
+        .then(countries => renderCountry (countries) )
+        .catch(error => console.log(error))
+    
+    };
 
-const fetchCountries = fetch('https://restcountries.com/v3.1/all').then(responce => {
-    return responce.json();
-}).then(country => {
-    console.log(country);
-})
+function fetchCountries (countryName) {
+  return  fetch(`https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`)
+    .then( (response) => {
+        if(!response.ok){
+            throw new Error(response.statusText);
+        }
+    return response.json();
+    })
+}
+function renderCountry(countries) {
+ return countries.map(({name,flags}) => {
+     return `<li class="gallery__item">
 
-console.log(fetchCountries);
+ <img class="country-img" src="${flags.svg}"
+   alt=""
+ /> <h2>${name.official}</h2>
+
+
+</li>`;}).join('')
+
+
+} 
+console.log();
+
+
+  
+   
+    
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
