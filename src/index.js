e12215a53bc85388928682086db0a00479257a61
Oleh Semennykov <sumcue@gmail.com>
2021-11-16 23:48:17 +0200

    renderCountry

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { debounce } from 'lodash';
+import Notiflix from 'notiflix';
 import './css/styles.css';
+
 
 
 const DEBOUNCE_DELAY = 300;
@@ -14,13 +16,26 @@ refs.searchBox.addEventListener('input', debounce((handleCountryInput),DEBOUNCE_
 
 function handleCountryInput(evt) {
     const countryName = evt.target.value;
+   
 fetchCountries(countryName)
-        .then(countries => renderCountry(countries) )
-        .catch(error => console.log(error))
+        .then(countries => { 
+            if(countries.length > 10){
+                Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+            } else if(countries.length >= 2 && countries.length <= 10){
+                renderCountries (countries)
+            } else{
+                renderCountry(countries)
+            }
+        })
+        .catch(error => Notiflix.Notify.failure("Oops, there is no country with that name"))
+        .finally(() => {
+            // refs.countryList.innerHTML = ""
+        })
         
     };
 
 function fetchCountries (countryName) {
+    
   return  fetch(`https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`)
     .then( (response) => {
         if(!response.ok){
@@ -41,7 +56,19 @@ function renderCountry(countries) {
  </li>`;}).join('')
 
 refs.countryList.innerHTML = markup;
+
 } 
+
+function renderCountries (countries) {
+const fewCountries = countries.map((country) => {
+    return `<li class="country-item">
+    <div class="country-part"><img class="country-img" src="${country.flags.svg}" width= "50mpx"  alt="${country.name.official}"/> 
+ <h2 class="country-name"><b>${country.name.official}</b></h2></div>
+ </li>`
+}).join('')
+refs.countryList.innerHTML = fewCountries;
+}
+ 
 
 
 
